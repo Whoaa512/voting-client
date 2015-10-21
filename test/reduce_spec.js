9d50ac907bcaa8c8cb62bab1b4d43a2a98cf13fa
C.J. Winslow <whoaa512@gmail.com>
2015-10-21 14:32:12 -0700

    add basic reducer

---
+++ test/reduce_spec.js
@@ -0,0 +1,65 @@
+require('./test_helper.js')
+import test from 'ava'
+import {expect} from 'chai'
+import {List, Map, fromJS} from 'immutable'
+import reducer from '../src/reducer'
+
+test('handles SET_STATE', t => {
+  const initialState = Map()
+  const action = {
+    type: 'SET_STATE',
+    state: fromJS({
+      vote: {
+        pair: ['Bacon', 'Bits'],
+        tally: { Bacon: 1 }
+      }
+    })
+  }
+  const nextState = reducer(initialState, action)
+  expect(nextState).to.equal(fromJS({
+    vote: {
+      pair: ['Bacon', 'Bits'],
+      tally: { Bacon: 1 }
+    }
+  }))
+})
+
+test('handles SET_STATE with plain JS payload', t => {
+  const initialState = Map()
+  const action = {
+    type: 'SET_STATE',
+    state: {
+      vote: {
+        pair: ['Bacon', 'Bits'],
+        tally: { Bacon: 1 }
+      }
+    }
+  }
+  const nextState = reducer(initialState, action)
+  expect(nextState).to.equal(fromJS({
+    vote: {
+      pair: ['Bacon', 'Bits'],
+      tally: { Bacon: 1 }
+    }
+  }))
+})
+
+test('handles SET_STATE without initial state', t => {
+  const action = {
+    type: 'SET_STATE',
+    state: {
+      vote: {
+        pair: ['Bacon', 'Bits'],
+        tally: { Bacon: 1 }
+      }
+    }
+  }
+  const nextState = reducer(undefined, action)
+  expect(nextState).to.equal(fromJS({
+    vote: {
+      pair: ['Bacon', 'Bits'],
+      tally: { Bacon: 1 }
+    }
+  }))
+})
+
