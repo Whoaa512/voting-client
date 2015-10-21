c0cc8fb1cf2154f4f5be5b47cfc9dc2d2763750c
C.J. Winslow <whoaa512@gmail.com>
2015-10-21 15:16:05 -0700

    fix null state for PureRenderComponent

--- src/components/PureRenderComponent.js
+++ src/components/PureRenderComponent.js
@@ -3,6 +3,7 @@ import shallowCompare from 'react-addons-shallow-compare'
 
 export default class PureRenderComponent extends React.Component {
   shouldComponentUpdate(nextProps, nextState) {
-    return !shallowCompare(this.props, nextProps) || !shallowCompare(this.state, nextState)
+    let state = this.state != null ? this.state : {}
+    return !shallowCompare(this.props, nextProps) || !shallowCompare(state, nextState)
   }
 }
